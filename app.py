e3e10741d7a6a80158b11fdeb4cf8a81c010c68b
Kartik Budhlakoti <kartikbudhlako@gmail.com>
2025-07-22 05:37:33 +0000

    Modifying for the tests

--- app.py
+++ app.py
@@ -62,5 +62,7 @@ def update(id):
         return render_template('update.html' , task=task)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
